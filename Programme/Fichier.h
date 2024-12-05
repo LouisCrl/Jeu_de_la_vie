80b7eec74495c252cb4f426c92d34a79050fed27
MajorBananaa <adrien.evrard@viacesi.fr>
2024-12-05 15:32:40 +0100

    Update Fichier.h

--- Programme/Fichier.h
+++ Programme/Fichier.h
@@ -2,14 +2,16 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include "Cell.h"
 
 class Fichier{
 
 private:
     std::string pass;
+    std::string new_pass;
     int ligne;
     int colonne;
-    std::vector<std::vector<bool>> donne;
+    std::vector<std::vector<Cell*>> donne;
 
 
 public:
@@ -17,5 +19,6 @@ public:
     void lire_fichier();
     bool stob(char);
     void get_grille_data();
+    void ecrire(int num);
 
 };
