28bcb8d67e5998a2a5d587e61ddf63aa69f38f0f
LouisCrl <louis.carlu@viacesi.fr>
2024-12-05 17:29:30 +0100

    Update Game.h

--- Programme/Game.h
+++ Programme/Game.h
@@ -2,14 +2,20 @@
 #define GAME_H
 
 #include "Grid.h"
+#include <string>
+
+using namespace std;
 
 class Game{
 private:
 
 public:
-    //void creer_grille();
-    virtual void print() = 0;
+    virtual void createGrid(string file);
+    virtual void createGrid();
+    virtual void print();
+    virtual void updateRender();
     virtual void iteration() = 0;
+    virtual ~Game();
     
 protected:
     Grid *grid;
