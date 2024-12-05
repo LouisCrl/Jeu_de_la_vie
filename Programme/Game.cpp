0b07f0884b3b28b9388cf59c9e8b7759d34ae776
LouisCrl <louis.carlu@viacesi.fr>
2024-12-05 17:29:50 +0100

    Update Game.cpp

---
+++ Programme/Game.cpp
@@ -0,0 +1,21 @@
+#include "Game.h"
+
+void Game::createGrid(string file){
+    return;
+}
+
+void Game::createGrid(){
+    return;
+}
+
+void Game::print(){
+    return;
+}
+
+void Game::updateRender(){
+    return;
+}
+
+Game::~Game(){
+    delete this->grid;
+}
