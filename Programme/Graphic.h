6a6887b78cad1ad34480ac8c74c067957f00fed7
LouisCrl <louis.carlu@viacesi.fr>
2024-12-06 14:08:39 +0100

    Update Graphic.h

--- Programme/Graphic.h
+++ Programme/Graphic.h
@@ -1,18 +1,21 @@
 #include "Game.h"
+#include <string>
 #include <SFML/Graphics.hpp>
+
+using namespace std;
 
 class Graphic : public Game{
 private:
-    long delay = 150;
+    long delay = 100;
     int cellSize;
     sf::RenderWindow window;
     bool running = true;
 
 public:
     Graphic();
-    Graphic(int cellSize, int l, int c);
+    Graphic(string pass_file, int cellSize);
     void createGrid();
-    void updateRender();
+    void updateRender(int l, int c);
     void iteration();
     ~Graphic();
 
