a6ffaf92a343fad04a5ce35fff72415def3aa0f3
MajorBananaa <adrien.evrard@viacesi.fr>
2024-12-06 09:45:33 +0100

    Update Grid.cpp

--- Programme/Grid.cpp
+++ Programme/Grid.cpp
@@ -2,15 +2,37 @@
 #include <iostream>
 
 Grid::Grid(){
-    vector<vector<Cell*>> grid(this->line, vector<Cell*>(this->column, new Cell())); 
-    initialiserGrille(5, 5, grid);
+    vector<vector<Cell*>> grid(5, vector<Cell*>(5, nullptr));
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            grid[i][j] = new Cell();
+
+        }
+    }
+    initGrid(5, 5, grid);
 }
 
 Grid::Grid(int line, int column, vector<vector<Cell*>> grid){
-    initialiserGrille(line, column, grid);
+    initGrid(line, column, grid);
 }
 
-void Grid::initialiserGrille(int line, int column, vector<vector<Cell*>> grid){
+Grid::Grid(const Grid& other){
+    this->line = other.line;
+    this->column = other.column;
+    this->grid.resize(this->line);
+    for (int i = 0; i < this->line; ++i) {
+        this->grid[i].resize(this->column);
+        for (int j = 0; j < this->column; ++j) {
+            if (other.grid[i][j] != nullptr) {
+                this->grid[i][j] = new Cell(*other.grid[i][j]);
+            } else {
+                this->grid[i][j] = nullptr;
+            }
+        }
+    }
+}
+
+void Grid::initGrid(int line, int column, vector<vector<Cell*>> grid){
     this->line = line;
     this->column = column;
     this->grid = grid;
@@ -28,7 +50,7 @@ vector<vector<Cell*>> Grid::getGrid(){
     return this->grid;
 }
 
-int Grid::verif(int l, int c, vector<vector<Cell*>> temp){
+int Grid::verif(int l, int c, Grid *temp){
     int count = 0;
     for (int i=-1; i<2; i++){
         for (int j=-1; j<2; j++){
@@ -37,7 +59,7 @@ int Grid::verif(int l, int c, vector<vector<Cell*>> temp){
             int neighborL = l + i;
             int neighborC = c + j;
             if (neighborL >= 0 && neighborL < this->line && neighborC >= 0 && neighborC < this->column){
-                if (temp[neighborL][neighborC]->estVivant()) count++;
+                if (temp->getGrid()[neighborL][neighborC]->estVivant()) count++;
             }
         }
     }
@@ -45,14 +67,14 @@ int Grid::verif(int l, int c, vector<vector<Cell*>> temp){
 }
 
 void Grid::update(){
-    vector<vector<Cell*>> temp = this->grid;
+    Grid temp = *this;
     int count;
     for (int i=0; i<this->line; i++){
         for (int j=0; j<this->column; j++){
-            count = verif(i, j, temp);
-            if (count == 3 && (!temp[i][j]->estVivant())){
+            count = verif(i, j, &temp);
+            if (count == 3 && (!temp.getGrid()[i][j]->estVivant())){
                 this->grid[i][j]->vie();
-            }else if ((count < 2 ||count > 3) && (temp[i][j]->estVivant())){
+            }else if ((count < 2 ||count > 3) && (temp.getGrid()[i][j]->estVivant())){
                 this->grid[i][j]->meurt();
             }
         }
