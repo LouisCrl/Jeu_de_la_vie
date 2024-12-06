ab00f754b78a310652684710698a21daf832c53a
MajorBananaa <adrien.evrard@viacesi.fr>
2024-12-06 10:47:22 +0100

    Update File.h

--- Programme/File.h
+++ Programme/File.h
@@ -1,10 +1,13 @@
+#ifndef FICHIER_H
+#define FICHIER_H
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include "Cell.h"
 
-class Fichier{
+class File{
 
 private:
     std::string pass;
@@ -15,10 +18,15 @@ private:
 
 
 public:
-    Fichier(std::string chemin);
+    File(std::string chemin);
     void lire_fichier();
     bool stob(char);
     void get_grille_data();
     void ecrire(int num);
-
+    int get_ligne();
+    int get_colonne();
+    std::vector<std::vector<Cell*>> get_fichier();
 };
+
+
+#endif
