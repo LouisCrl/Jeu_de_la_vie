6bed9d733fb771fc7f44a2f59542f52921fa8905
MajorBananaa <adrien.evrard@viacesi.fr>
2024-12-06 09:45:52 +0100

    Update Grid.h

--- Programme/Grid.h
+++ Programme/Grid.h
@@ -8,19 +8,22 @@ class Grid{
 private:
     int line;
     int column;
-    vector<vector<Cell*>> grid;
-    void initialiserGrille(int line, int column,vector<vector<Cell*>> grid);
+    
+    void initGrid(int line, int column,vector<vector<Cell*>> grid);
 
 public:
     Grid();
     Grid(int line, int column, vector<vector<Cell*>> grid);
+    Grid(const Grid& other);
     int getLine();
     int getColumn();
     vector<vector<Cell*>> getGrid();
-    int verif(int l, int c, vector<vector<Cell*>> temp);
+    int verif(int l, int c, Grid* temp);
     void update();
     ~Grid();
+    vector<vector<Cell*>> grid;
 
 protected:
+
 
 };
