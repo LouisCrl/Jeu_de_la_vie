aa493aa79490a49cc2bbb14b94a7bdaedf8854b0
MajorBananaa <adrien.evrard@viacesi.fr>
2024-12-06 21:29:21 +0100

    Update main.cpp

--- Programme/main.cpp
+++ Programme/main.cpp
@@ -1,12 +1,36 @@
 #include "Console.h"
 #include "Graphic.h"
+#include <string>
+#include <iostream>
 
 int main(){
-    Game *c = new Graphic("test.txt", 8);
-    
-    c->iteration();
-    
-    delete c;
 
+    std::string pass_file;
+    std::cout << "entrez le chemin du fichier contenant les données : " <<  std::endl;
+    std::cin >> pass_file;
+
+    std::cout << "  __________  " <<  std::endl;
+    std::cout << "  __________  " <<  std::endl;
+
+    int choice;
+    std::cout << "Choix de l'affichage : " <<  std::endl;
+    std::cout << "1 = Affichage Console" <<  std::endl;
+    std::cout << "2 = Affichage Graphique" <<  std::endl;
+    std::cin >> choice;
+
+    if(choice == 1){
+
+        int iteration;
+        std::cout << "Entrez le nombre d'itération " <<  std::endl;
+        std::cin >> iteration;
+        Game *c = new Console(pass_file, iteration);
+        c->iteration();
+        delete c;
+        return 0;
+    }
+    Game *c = new Graphic(pass_file, 8);
+    c->iteration();
+    delete c;
     return 0;
+
 }
