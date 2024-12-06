4aaf272b3bfb0c9a93d3b3d81e892fffdafd42ee
MajorBananaa <adrien.evrard@viacesi.fr>
2024-12-06 09:44:52 +0100

    Update Game.cpp

---
+++ Programme/Game.cpp
@@ -0,0 +1,17 @@
+#include "Game.h"
+
+void Game::createGrid(){
+    return;
+}
+
+void Game::print(){
+    return;
+}
+
+void Game::updateRender(){
+    return;
+}
+
+Game::~Game(){
+    delete this->grid;
+}
