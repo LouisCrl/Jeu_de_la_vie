4782b01204fbe12969d2593e2816e7e2cac534d2
LouisCrl <louis.carlu@viacesi.fr>
2024-12-06 14:08:18 +0100

    Update Graphic.cpp

--- Programme/Graphic.cpp
+++ Programme/Graphic.cpp
@@ -1,6 +1,5 @@
 #include "Graphic.h"
-
-using namespace std;
+#include <iostream>
 
 Graphic::Graphic(){
     this->cellSize = 100;
@@ -9,20 +8,16 @@ Graphic::Graphic(){
     createGrid();
 }
 
-Graphic::Graphic(int cellSize, int l, int c){
+Graphic::Graphic(string pass_file, int cellSize){
     this->cellSize = cellSize;
-    vector<vector<Cell*>> vide(l, vector<Cell*>(c, nullptr));
-    for (int i = 0; i < l; ++i) {
-        for (int j = 0; j < c; ++j) {
-            vide[i][j] = new Cell();
-        }
-    }
-    this->grid = new Grid(l, c, vide);
+    this->file = new File(pass_file);
+    this->grid = new Grid(file->get_ligne(), file->get_colonne(), file->get_fichier());
     this->window.create(sf::VideoMode(this->grid->getLine() * this->cellSize + 30, this->grid->getColumn() * this->cellSize), "Game of Life");
     createGrid();
 }
 
 void Graphic::createGrid(){
+    updateRender(this->grid->getLine(), this->grid->getColumn());
     bool start = false;
     sf::Event event;
     sf::RectangleShape cell(sf::Vector2f(cellSize-1.0f, cellSize-1.0f));
@@ -63,19 +58,14 @@ void Graphic::createGrid(){
     }
 }
 
-void Graphic::updateRender(){
+void Graphic::updateRender(int l, int c){
+    vector<vector<Cell*>> g = this->grid->getGrid();
     sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
-    for (int i=0; i<this->grid->getLine(); i++){
-        for (int j=0; j<this->grid->getColumn(); j++){
-            if (this->grid->getGrid()[i][j]->estVivant()){
-                cell.setPosition(i*cellSize, j*cellSize);
-                cell.setFillColor(sf::Color::Green);
-                this->window.draw(cell);
-            }else{
-                cell.setPosition(i*cellSize, j*cellSize);
-                cell.setFillColor(sf::Color::Black);
-                this->window.draw(cell);
-            }
+    for (int i=0; i<l; i++){
+        for (int j=0; j<c; j++){
+            cell.setPosition(i*cellSize, j*cellSize);
+            cell.setFillColor(g[i][j]->estVivant() ? sf::Color::Green : sf::Color::Black);
+            this->window.draw(cell);
         }
     }
     this->window.display();
@@ -92,6 +82,9 @@ void Graphic::iteration(){
     gaugeProgress.setFillColor(sf::Color::Blue);
     gaugeProgress.setPosition(this->grid->getLine() * this->cellSize, this->grid->getColumn() * this->cellSize);
     
+    int l = this->grid->getLine();
+    int c = this->grid->getColumn();
+
     while (this->running && this->window.isOpen()) {
         sf::Event event;
         while (this->window.pollEvent(event)) {
@@ -107,12 +100,12 @@ void Graphic::iteration(){
                 }
                 if (event.key.code == sf::Keyboard::Down){
                     if (this->delay < 500){
-                        this->delay += 50.0f;
+                        this->delay += 20.0f;
                     }
                 }
                 if (event.key.code == sf::Keyboard::Up){
-                    if (this->delay > 50){
-                        this->delay -= 50.0f;
+                    if (this->delay > 20){
+                        this->delay -= 20.0f;
                     }
                 }
             }
@@ -127,7 +120,7 @@ void Graphic::iteration(){
         if (this->running && (!pause)){            
             this->grid->update();
 
-            updateRender();
+            updateRender(l, c);
 
             sf::sleep(sf::milliseconds(this->delay));
         }
