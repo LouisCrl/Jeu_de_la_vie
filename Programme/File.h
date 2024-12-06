315bfc5822bb6489ebdf0b49ee77720ed6c20103
MajorBananaa <adrien.evrard@viacesi.fr>
2024-12-06 09:43:57 +0100

    Update and rename Fichier.h to File.h

---
+++ Programme/File.h
@@ -0,0 +1,32 @@
+#ifndef FICHIER_H
+#define FICHIER_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "Cell.h"
+
+class File{
+
+private:
+    std::string pass;
+    std::string new_pass;
+    int ligne;
+    int colonne;
+    std::vector<std::vector<Cell*>> donne;
+
+
+public:
+    File(std::string chemin);
+    void lire_fichier();
+    bool stob(char);
+    void get_grille_data();
+    void ecrire(int num);
+    int get_ligne();
+    int get_colonne();
+    std::vector<std::vector<Cell*>> get_fichier();
+};
+
+
+#endif
