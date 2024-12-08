59408588537db416caf5d6bac6b31e330655984c
LouisCrl <louis.carlu@viacesi.fr>
2024-12-08 17:56:11 +0100

    Update main.cpp

--- Programme/main.cpp
+++ Programme/main.cpp
@@ -1,10 +1,8 @@
-#include "Console.h"
-#include "Graphic.h"
+#include "View.h"
 #include <string>
 #include <iostream>
 
 int main(){
-
     std::string pass_file;
     std::cout << "entrez le chemin du fichier contenant les données : " <<  std::endl;
     std::cin >> pass_file;
@@ -18,19 +16,9 @@ int main(){
     std::cout << "2 = Affichage Graphique" <<  std::endl;
     std::cin >> choice;
 
-    if(choice == 1){
+    View v(choice, pass_file);
 
-        int iteration;
-        std::cout << "Entrez le nombre d'itération " <<  std::endl;
-        std::cin >> iteration;
-        Game *c = new Console(pass_file, iteration);
-        c->iteration();
-        delete c;
-        return 0;
-    }
-    Game *c = new Graphic(pass_file, 8);
-    c->iteration();
-    delete c;
+    v.lancer_view();
+    
     return 0;
-
 }
