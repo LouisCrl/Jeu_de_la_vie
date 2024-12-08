b13fcc5090ecbc6f8acc787f7f76bd74a7037ebe
LouisCrl <louis.carlu@viacesi.fr>
2024-12-08 23:48:22 +0100

    Update Graphic.h

--- Programme/Graphic.h
+++ Programme/Graphic.h
@@ -6,17 +6,17 @@ using namespace std;
 
 class Graphic : public Game{
 private:
-    long delay = 100;
-    int cellSize;
-    sf::RenderWindow window;
-    bool running = true;
+    long delay = 100;                           //délai entre 2 itérations de base à 100 milliseconds
+    int cellSize;                               //taille des cellules
+    sf::RenderWindow window;                    //fenêtre affichée
+    bool running = true;                        //booléen pour savoir si le programme tourne
 
 public:
-    Graphic();
-    Graphic(string pass_file, int cellSize);
-    void createGrid();
-    void updateRender(int l, int c);
-    void iteration();
+    Graphic();                                  //Constructeur par défaut
+    Graphic(string pass_file, int cellSize);    //Constructeur
+    void createGrid();                          //Création de la grille graphique
+    void updateRender(int l, int c);            //Actualisation de la grille graphique
+    void iteration();                           //Boucle des itérations
     ~Graphic();
 
 protected:
