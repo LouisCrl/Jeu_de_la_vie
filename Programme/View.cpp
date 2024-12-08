1d25e45ace53dcce9e8c4d4a6989d6e51845856b
LouisCrl <louis.carlu@viacesi.fr>
2024-12-08 17:54:08 +0100

    Update View.cpp

--- Programme/View.cpp
+++ Programme/View.cpp
@@ -3,27 +3,22 @@
 View::View(int choice, string pass_file){
     this -> choice = choice;
     this -> pass_file = pass_file;
-
 }
 void View::lancer_view(){
-    if(thischoice == 1){
-        Controleur *c = new Controleur();
+    Controller *c = new Controller();
+    if(this->choice == 1){
         int nb_iteration;
         std::cout << "Entrez le nombre d'itération " <<  std::endl;
-        std::cin << nb_iteration;
+        std::cin >> nb_iteration;
 
         c -> createGameConsole(this -> pass_file, nb_iteration);
-        c -> play();
-        c -> deleteGame();
     }
-    else(){
-        Controleur *c = new Controleur();
-
+    else{
         int cell_size;
         std::cout << "Entrez la taille des cellules " <<  std::endl;
-        std::cin << cell_size;
+        std::cin >> cell_size;
         c -> createGameGraphic(this -> pass_file, cell_size);
-        c -> play();
-        c -> deleteGame();
     }
+    c -> play();
+    c -> deleteGame();
 }
